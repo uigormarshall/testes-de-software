c05aecaff9bb65d6d6eff1c1fef46125af076c1b
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 12:56:53 -0300

    Adiciona Somar_RetornarValoresSomados usando Theory

--- Demo.tests/CalculadoraTests.cs
+++ Demo.tests/CalculadoraTests.cs
@@ -8,7 +8,7 @@ namespace Demo.tests
     public class CalculadoraTests
     {
         [Fact]
-        public void Somar_RetorvarValorSoma()
+        public void Somar_RetornarValorSoma()
         {
             // Arrange
             var calculadora = new Calculadora();
@@ -18,6 +18,24 @@ namespace Demo.tests
 
             // Assert
             Assert.Equal(4, resultado);
+        }
+
+        [Theory]
+        [InlineData(1, 1 ,2)]
+        [InlineData(1, 2, 3)]
+        [InlineData(1, 3, 4)]
+        [InlineData(1, 5, 6)]
+        [InlineData(500, 99, 599)]
+        public void Somar_RetornarValoresSomados(double valor1, double valor2, double total)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(valor1, valor2);
+
+            // Assert
+            Assert.Equal(total, resultado);
         }
     }
 }
