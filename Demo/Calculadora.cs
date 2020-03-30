68199bf6da7efe5a9e120d4eb84befb2ab91a38f
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 12:51:22 -0300

    Adiciona Classe Calculadora

---
+++ Demo/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double valor1, double valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        public double Dividir(double valor1, double valor2)
+        {
+            return valor1 / valor2;
+        }
+    }
+}
