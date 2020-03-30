af01b0fc7f0f0cf4d0c6fd09a3e49fec4aafe8d5
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 12:51:57 -0300

    Adiciona CalculadoraTest

---
+++ Demo.tests/CalculadoraTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Demo.tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Somar_RetorvarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(2, 2);
+
+            // Assert
+            Assert.Equal(41, resultado);
+        }
+    }
+}
