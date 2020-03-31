45a8dfb4278dac4d804e2da501ef99bd69a629fc
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 23:39:15 -0300

    Adiciona Assercao de Excecoes

---
+++ Demo.tests/08 - AssercaoExcecaoTestes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xunit;
+
+namespace Demo.tests
+{
+    public class AssercaoExcecaoTestes
+    {
+        [Fact]
+        public void Calculadora_Dividir_DeveRetornarErroDivisaoPorZero()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act & Assert
+            Assert.Throws<DivideByZeroException>(() => calculadora.Dividir(10, 0));
+        }
+
+        [Fact]
+        public void Funcionario_Salario_DeveRetornarErroSalarioInferiorAoPermitido()
+        {
+            // Arrange & Act & Assert
+            var excecao = Assert.Throws<Exception>(() => FuncionarioFactory.Criar("João Vitor", 250));
+            Assert.Equal("Salario inferior ao permitido", excecao.Message);
+        }
+    }
+}
