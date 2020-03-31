11b076816d63aae94834e140204c4c7c65370fc0
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 23:32:02 -0300

    Adiciona Assercao de Colecoes

---
+++ Demo.tests/07 - AssercaoColecoesTestes.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+
+namespace Demo.tests
+{
+    public class AssercaoColecoesTestes
+    {
+        [Fact]
+        public void Funcionario_Habilidades_NaoDevePossuirHabilidadesVazias()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Emilly S. Fonseca", 1000);
+
+            // Assert
+            Assert.All(funcionario.Habilidades, habilidade => Assert.False(string.IsNullOrWhiteSpace(habilidade)));
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_JuniorNaoDevePossuirHabilidadesAvancadas()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Emilly S. Fonseca", 1000);
+
+            // Assert
+            Assert.DoesNotContain("Microservices", funcionario.Habilidades);
+        }
+
+        [Fact]
+        public void Funcionario_Habilidades_SeniorDevePossuirTodasHabilidades()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Efraim Santana", 9000);
+
+            var habilidadesAvancadas = new[]
+            {
+                "Lógica de Programação",
+                "POO",
+                "Testes",
+                "Microservices"
+            };
+            // Assert
+            Assert.Equal(habilidadesAvancadas, funcionario.Habilidades);
+        }
+    }
+}
