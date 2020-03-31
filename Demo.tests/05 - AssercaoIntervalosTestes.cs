8d8ccbba423d7e9c3d615b953d029245904de42e
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 23:13:29 -0300

    Adiciona Assercao de Intervalos

---
+++ Demo.tests/05 - AssercaoIntervalosTestes.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Demo.tests
+{
+    public class AssercaoIntervalosTestes
+    {
+        [Theory]
+        [InlineData(700)]
+        [InlineData(1500)]
+        [InlineData(2000)]
+        [InlineData(7500)]
+        [InlineData(8000)]
+        [InlineData(1500)]
+        public void Funcionario_Salario_FaixasSalariaisDevemRespeitarNivelProfissional(double salario)
+        {
+            // Arrange & Act
+            var funcionario = new Funcionario("Rafaela Leal", salario);
+
+            // Assert
+            if(funcionario.NivelProfissional == NivelProfissional.Junior)
+                Assert.InRange(funcionario.Salario, 500, 1999);
+
+            if (funcionario.NivelProfissional == NivelProfissional.Pleno)
+                Assert.InRange(funcionario.Salario, 2000, 7999);
+
+            if (funcionario.NivelProfissional == NivelProfissional.Senior)
+                Assert.InRange(funcionario.Salario, 8000, double.MaxValue);
+
+            Assert.NotInRange(funcionario.Salario, 0, 499);
+        }
+    }
+}
