c44203ceeee5c869f7f5ca55e39188c097989001
Uigor Marshall <uigor.silva@acad.ifma.edu.br>
2020-03-30 22:24:11 -0300

    Adiciona classes para testes de asserção

---
+++ Demo/Pessoa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+    }
+}
---
+++ Demo/StringTools.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public static class StringTools
+    {
+        public static string Unir(string texto1, string texto2)
+        {
+            return $"{texto1} {texto2}";
+        }
+    }
+}
